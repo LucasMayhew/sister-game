94a16e5d3f3cb3acb5c9e47100d9a6cb2fc8ec59
LucasMayhew <59377840+LucasMayhew@users.noreply.github.com>
2020-07-04 11:56:44 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -307,7 +307,7 @@ d d d d e e e e e e e e d d d d
 d d d d e e e e e e e e d d d d 
 `
 }
-function doSomething () {
+function cpus () {
     dad = sprites.create(img`
 . . . . e e e e e e . . . . . . 
 . . . e d d d d d d e . . . . . 
@@ -349,15 +349,58 @@ function doSomething () {
     sprites.setDataBoolean(mom, "mom", true)
     tiles.placeOnTile(mom, tiles.getTileLocation(8, 8))
     dad.setFlag(SpriteFlag.Invisible, true)
+    dad.setFlag(SpriteFlag.Ghost, true)
     mom.setFlag(SpriteFlag.Invisible, true)
+    mom.setFlag(SpriteFlag.Ghost, true)
+}
+function Quest2 (mySprite: Sprite, mySprite2: Sprite) {
+    if (Quest == "Quest 2") {
+        if (sprites.readDataBoolean(mySprite, "mom")) {
+            game.splash("" + name + " go water the flowers.")
+            game.showLongText("Quest go water the flowers.", DialogLayout.Top)
+            pause(500)
+        } else {
+            if (sprites.readDataBoolean(mySprite, "dad")) {
+                game.splash("" + name + " go water the flowers.")
+                game.showLongText("Quest go water the flowers.", DialogLayout.Top)
+                pause(500)
+            }
+        }
+    }
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.cpu, function (sprite, otherSprite) {
     if (Quest == "Quest 1") {
         if (sprites.readDataBoolean(otherSprite, "house giy")) {
-            game.splash("" + name + " go git your mom and dad")
+            game.splash("" + name + " go get your mom and dad")
             game.showLongText("Quest get mom and dad", DialogLayout.Top)
             pause(500)
+        } else {
+            if (sprites.readDataBoolean(otherSprite, "mom")) {
+                game.splash("what Tom`s house was destroyed.")
+                otherSprite.setFlag(SpriteFlag.Invisible, true)
+                otherSprite.setFlag(SpriteFlag.Ghost, true)
+                dad.setFlag(SpriteFlag.Invisible, true)
+                dad.setFlag(SpriteFlag.Ghost, true)
+                tiles.placeOnTile(mom, tiles.getTileLocation(24, 5))
+                tiles.placeOnTile(dad, tiles.getTileLocation(23, 5))
+                Quest = "Quest 2"
+                pause(500)
+            } else {
+                if (sprites.readDataBoolean(otherSprite, "dad")) {
+                    game.splash("what Tom`s house was destroyed.")
+                    otherSprite.setFlag(SpriteFlag.Invisible, true)
+                    otherSprite.setFlag(SpriteFlag.Ghost, true)
+                    dad.setFlag(SpriteFlag.Invisible, true)
+                    dad.setFlag(SpriteFlag.Ghost, true)
+                    tiles.placeOnTile(mom, tiles.getTileLocation(24, 5))
+                    tiles.placeOnTile(dad, tiles.getTileLocation(23, 5))
+                    Quest = "Quest 2"
+                    pause(500)
+                }
+            }
         }
+    } else {
+        Quest2(otherSprite, sprite)
     }
 })
 function start () {
@@ -460,15 +503,13 @@ b 1 b 1 1 b b 1 1 b b 1 1 b b 1 1 b b 1 1 b b b
     statusbar.setColor(2, 15, 4)
     statusbar.setLabel("HP")
     statusbar.attachToSprite(mySprite)
-    doSomething()
 }
 function house () {
     if (mySprite.tileKindAt(TileDirection.Top, myTiles.tile3)) {
         if (controller.A.isPressed()) {
             mySprite2.setFlag(SpriteFlag.Invisible, true)
+            mySprite2.setFlag(SpriteFlag.Ghost, true)
             tiles.placeOnTile(mySprite, tiles.getTileLocation(6, 5))
-            dad.setFlag(SpriteFlag.Invisible, false)
-            mom.setFlag(SpriteFlag.Invisible, false)
             tiles.setTilemap(tiles.createTilemap(
             hex`10001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001918181a181818000000000000000000191c1c1c1c1c19000000000000000000191c0f10111c19000000000000000000191c1217161c19000000000000000000191c1217161c19000000000000000000191c1217161c19000000000000000000191c1217161c19000000000000000000191c1314151c19000000000000000000191d1c1c1c1c19000000000000000000181818181818180000000000000000000000000000000000000000000000000000000000000000000000000000`,
             img`
@@ -493,12 +534,22 @@ function house () {
             TileScale.Sixteen
         ))
             scene.setBackgroundColor(15)
+            if (Quest == "Quest 1") {
+                dad.setFlag(SpriteFlag.Invisible, false)
+                dad.setFlag(SpriteFlag.Ghost, false)
+                mom.setFlag(SpriteFlag.Invisible, false)
+                mom.setFlag(SpriteFlag.Ghost, false)
+            } else {
+                dad.setFlag(SpriteFlag.Invisible, true)
+                dad.setFlag(SpriteFlag.Ghost, true)
+                mom.setFlag(SpriteFlag.Invisible, true)
+                mom.setFlag(SpriteFlag.Ghost, true)
+            }
             pause(1000)
         }
     } else {
         if (mySprite.tileKindAt(TileDirection.Top, myTiles.tile16)) {
             if (controller.A.isPressed()) {
-                mySprite2.setFlag(SpriteFlag.Invisible, false)
                 tiles.setTilemap(tiles.createTilemap(
             hex`1e001000050505050505090a05050505050505050e0e0e0e0e0e0e0e0e0e0e0e0e0e1515151515151717151515151515151515151515151515151515151515151410141414140708000100000100000000000000000000000000001c1c1c0000000100000708000100000000000000000000001800111213001c1c1c00000000000007060c0c0c0c0c0c0c0c0c0c0c0c0c0c00000000001c1c1c000e000000000b0d0d0d0d0d0d0d0d0d0d0d0d0d0d0d00000000001c1c1c0000000000000000000000000001000000000000000000000000001c1c1c00000000010000000e0000000000000000000000000000000000001c1c1c0000000100000000000005010000000000000000000000000000001c1c1c0404040404040400000000000000000000000000000000000000001c1c1c0303030303030404040404040404040404040404000000000000001d1d1d0303030303030303030303030303030303030304040404040404041c1c1c030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303`,
             img`
@@ -524,10 +575,26 @@ function house () {
         ))
                 scene.setBackgroundColor(7)
                 tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 3))
+                mySprite2.setFlag(SpriteFlag.Invisible, false)
+                mySprite2.setFlag(SpriteFlag.Ghost, false)
+                if (Quest == "Quest 1") {
+                    dad.setFlag(SpriteFlag.Invisible, true)
+                    dad.setFlag(SpriteFlag.Ghost, true)
+                    mom.setFlag(SpriteFlag.Invisible, true)
+                    mom.setFlag(SpriteFlag.Ghost, true)
+                } else {
+                    dad.setFlag(SpriteFlag.Invisible, false)
+                    dad.setFlag(SpriteFlag.Ghost, false)
+                    mom.setFlag(SpriteFlag.Invisible, false)
+                    mom.setFlag(SpriteFlag.Ghost, false)
+                }
                 pause(1000)
             }
         }
     }
+}
+function tilemaps (myImage: Image, mySprite: Sprite) {
+	
 }
 let statusbar: StatusBarSprite = null
 let mySprite2: Sprite = null
@@ -537,8 +604,10 @@ let Quest = ""
 let mom: Sprite = null
 let dad: Sprite = null
 start()
+cpus()
 forever(function () {
     house()
+    tilemaps(myTiles.tile17, mySprite)
 })
 forever(function () {
     music.playTone(880, music.beat(BeatFraction.Half))
